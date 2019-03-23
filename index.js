af2b6ff039b33e64b60b4da0236acc471ca80eb8
nfrisbee <nicolefrisbee@yahoo.com>
2019-03-23 13:54:05 -0600

    strange issues with the auth token

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const SlackBot = require('slackbots');
 const axios = require('axios');
 
 const bot = new SlackBot({
-    token: 'xoxb-577236061777-585935027188-zhY1qLHs15HT9J1UZzCgJwbc',
+    token: 'xoxb-577236061777-585935027188-grDzv2IxAGgLcQRCUCHcxMtV',
     name: 'PortBot'
 });
 
@@ -14,3 +14,18 @@ bot.on('start', () => {
 
     bot.postMessageToChannel('portbot', 'Ring Ring!', params);
 });
+
+// Error handler
+bot.on('error', (err) => console.log(err));
+
+// Message Handler
+bot.on('message', data => {
+    if (data.type !== 'message') {
+        return;
+    }
+
+    console.log(data);
+});
+/*we need to set this up in Heroku I believe. 
+The auth token changes every time, and I have to re-copy and paste the new auth code every time I run npm start. Strange, but we'll figure out the solution.
+*/
