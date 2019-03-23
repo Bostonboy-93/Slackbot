dbd8432444cd39eedb93414db3a6d3001265f92b
nfrisbee <nicolefrisbee@yahoo.com>
2019-03-23 13:20:53 -0600

    added auth token -NF

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-test
+const SlackBot = require('slackbots');
+const axios = require('axios');
+
+const bot = new SlackBot({
+    token: 'xoxb-577236061777-585935027188-0h6iY4JtMBiD0IKWiBRUMYhv',
+    name: 'PortBot'
+});
